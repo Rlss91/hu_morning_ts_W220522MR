9afa55ebf3324c1fae94228f11adcbf1b3ea5a17
Rlss91 <rlss91@gmail.com>
2022-08-25 16:03:12 +0300

    hw

--- realestate_project_ts/src/models/User.model.js
+++ realestate_project_ts/src/models/User.model.js
@@ -13,15 +13,3 @@ export class User {
     }
 }
 let user = new User("asdf", "asdf", "asdf", "asdf", 5);
-function app() {
-    var title = "Title!!";
-    function printTitle() {
-        console.log("title", title);
-    }
-    function calc() { }
-    var a;
-    var b, { return: a };
-    ++ + b;
-}
-printTitle("hello world");
-console.log(calc(5, "a"));
